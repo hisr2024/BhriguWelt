b58d14d9c1557fca5cf81446170cce98feb02a6e
hisr2024 <hisr2024@gmail.com>
2026-01-09 13:00:23 +0100

    Allow Google Fonts in CSP

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -26,9 +26,9 @@ const nextConfig = {
     const contentSecurityPolicy = [
       "default-src 'self'",
       "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
-      "style-src 'self' 'unsafe-inline'",
+      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "img-src 'self' data: https:",
-      "font-src 'self' data:",
+      "font-src 'self' data: https://fonts.gstatic.com",
       "connect-src 'self' http: https:",
       "frame-ancestors 'none'",
       "base-uri 'self'",
